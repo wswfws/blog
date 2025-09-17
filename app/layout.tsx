6fce66351c523c21c0bb1fd060b0a7d5c9d84344
wswfws <wswfws@ya.ru>
2025-09-17 14:37:12 +0500

    add Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
         <body>
         {children}
         <Footer />
+        <Analytics />
         </body>
         </html>
     );
