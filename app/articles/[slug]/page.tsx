69b1bb399eccc06e16081f4a69b8e048e59cd457
wswfws <wswfws@ya.ru>
2025-12-21 17:49:08 +0500

    update ArticlePage to handle async params and display article title

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,7 +1,12 @@
-export default function ArticlePage() {
-    return (
-        <main className="container mx-auto px-4 py-10 text-gray-200">
+export default async function ArticlePage({params}: {
+  params: Promise<{ slug: string }>
+}) {
 
-        </main>
-    );
+  const { slug } = await params;
+
+  return (
+    <main className="container mx-auto px-4 py-10 text-gray-200">
+      <h1 className="text-3xl font-bold mb-6">Статья: {slug}</h1>
+    </main>
+  );
 }
