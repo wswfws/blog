1b5d8e79e29ebb96a12e8d924ea983a5fe4587e3
wswfws <wswfws@ya.ru>
2026-01-12 13:03:47 +0500

    add new article on GPT as a tool and update article list

--- articles/index.ts
+++ articles/index.ts
@@ -2,11 +2,13 @@ import IArticle from "@/types/IArticle";
 import whyLazyToWriteBlogPost from "@/articles/why-lazy-to-write-blog-post";
 import whyWriteArticleIfLazy from "@/articles/why-write-article-if-lazy";
 import studentsDrivingGPT from "@/articles/students-driving-gpt-search-growth";
+import gptIsTool from "@/articles/gpt-is-tool";
 
 const allPosts: IArticle[] = [
   whyLazyToWriteBlogPost,
   whyWriteArticleIfLazy,
-  studentsDrivingGPT
+  studentsDrivingGPT,
+  gptIsTool
 ];
 
 export default allPosts;
